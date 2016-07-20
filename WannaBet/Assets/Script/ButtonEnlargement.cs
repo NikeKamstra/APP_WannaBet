0c1df3de16294b4fc82690c661e1e25a55f0f7e4
NikeKamstra <nike_kam@live.nl>
2016-07-20 15:58:56 +0200

    screen buttons layout

---
+++ WannaBet/Assets/Script/ButtonEnlargement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonEnlargement : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Enlarge()
+    {
+        RectTransform RectTransform = gameObject.GetComponent<RectTransform>();
+        RectTransform.anchorMin -= new Vector2(0.005f, 0);
+        RectTransform.anchorMax += new Vector2(0.005f, 0);
+    }
+}
